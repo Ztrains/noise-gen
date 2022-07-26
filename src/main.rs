76425bbd7331af6a86fd4dc3f4751e3e333d19d2
Ztrains <zachary.t.rich@gmail.com>
2022-07-26 17:00:10 -0500

    convert naming to use rust naming conventions

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Vector2D {
     y: f32
 }
 
-fn noise2D(x: f32, y: f32, permTable: [u32; SIZE*2], gradTable: [Vector2D; SIZE]) -> f32 {
+fn noise_2d(x: f32, y: f32, perm_table: [u32; SIZE*2], grad_table: [Vector2D; SIZE]) -> f32 {
 
     //let permTable: [u32; SIZE*2] = createPermutationTable();
     //let gradTable: [Vector2D; SIZE] = createGradientTable();
@@ -29,61 +29,61 @@ fn noise2D(x: f32, y: f32, permTable: [u32; SIZE*2], gradTable: [Vector2D; SIZE]
 
 
     // calculate vectors from each grid corner to point (x,y)
-    let vecBotLeft: Vector2D = Vector2D {
+    let vec_bot_left: Vector2D = Vector2D {
         x: x - x0 as f32,
         y: y - y0 as f32
     };
-    let vecBotRight: Vector2D = Vector2D {
+    let vec_bot_right: Vector2D = Vector2D {
         x: x - x1 as f32,
         y: y - y0 as f32
     };
-    let vecTopLeft: Vector2D = Vector2D {
+    let vec_top_left: Vector2D = Vector2D {
         x: x - x0 as f32,
         y: y - y1 as f32
     };
-    let vecTopRight: Vector2D = Vector2D {
+    let vec_top_right: Vector2D = Vector2D {
         x: x - x1 as f32,
         y: y - y1 as f32
     };
 
 
     // get pseudorandom value (hash) from permutation table for each grid corner
-    let valBotLeft: u32 = permTable[permTable[x0] as usize + y0];
-    let valBotRight: u32 = permTable[permTable[x1] as usize + y0];
-    let valTopLeft: u32 = permTable[permTable[x0] as usize + y1];
-    let valTopRight: u32 = permTable[permTable[x1] as usize + y1];
+    let val_bot_left: u32 = perm_table[perm_table[x0] as usize + y0];
+    let val_bot_right: u32 = perm_table[perm_table[x1] as usize + y0];
+    let val_top_left: u32 = perm_table[perm_table[x0] as usize + y1];
+    let val_top_right: u32 = perm_table[perm_table[x1] as usize + y1];
 
     // get associated gradient for each hash
-    let gradBotLeft: Vector2D = gradTable[valBotLeft as usize];
-    let gradBotRight: Vector2D = gradTable[valBotRight as usize];
-    let gradTopLeft: Vector2D = gradTable[valTopLeft as usize];
-    let gradTopRight: Vector2D = gradTable[valTopRight as usize];
+    let grad_bot_left: Vector2D = grad_table[val_bot_left as usize];
+    let grad_bot_right: Vector2D = grad_table[val_bot_right as usize];
+    let grad_top_left: Vector2D = grad_table[val_top_left as usize];
+    let grad_top_right: Vector2D = grad_table[val_top_right as usize];
 
 
     // calculate dot product of gradient and vector for each grid corner
-    let dotBotLeft = dot(vecBotLeft, gradBotLeft);
-    let dotBotRight = dot(vecBotRight, gradBotRight);
-    let dotTopLeft = dot(vecTopLeft, gradTopLeft);
-    let dotTopRight = dot(vecTopRight, gradTopRight);
+    let dot_bot_left = dot(vec_bot_left, grad_bot_left);
+    let dot_bot_right = dot(vec_bot_right, grad_bot_right);
+    let dot_top_left = dot(vec_top_left, grad_top_left);
+    let dot_top_right = dot(vec_top_right, grad_top_right);
 
     // calculate interpolation weights
     let dx = x - x.floor();
     let dy = y - y.floor();
 
     // apply fade to weights
-    let fadeX = fade(dx);
-    let fadeY = fade(dy);
+    let fade_x = fade(dx);
+    let fade_y = fade(dy);
 
     // perform linear interpolation for left and right sides of grid
-    let leftLerp = lerp(fadeY, dotBotLeft, dotTopLeft);
-    let rightLerp = lerp(fadeY, dotBotRight, dotTopRight);
+    let left_lerp = lerp(fade_y, dot_bot_left, dot_top_left);
+    let right_lerp = lerp(fade_y, dot_bot_right, dot_top_right);
 
     // perform final linear interpolation to get end value
-    lerp(fadeX, leftLerp, rightLerp)
+    lerp(fade_x, left_lerp, right_lerp)
 
 }
 
-fn createPermutationTable() -> [u32; SIZE*2] {
+fn create_permutation_table() -> [u32; SIZE*2] {
     let mut table: [u32; SIZE*2] = [0; SIZE*2];
     for index in 0..SIZE {
         table[index] = index as u32;
@@ -107,7 +107,7 @@ fn createPermutationTable() -> [u32; SIZE*2] {
 
 // another option to consider for generating random gradients:
 // roll a single number between 0 and 2π. Call it θ. Your vector is (cos θ, sin θ). 
-fn createGradientTable() -> [Vector2D; SIZE] {
+fn create_gradient_table() -> [Vector2D; SIZE] {
     let mut table: [Vector2D; SIZE] = [Vector2D{x: 0.0, y: 0.0}; SIZE];
     let mut rng = rand::thread_rng();
 
@@ -120,9 +120,9 @@ fn createGradientTable() -> [Vector2D; SIZE] {
         };
 
         // then normalize the vector
-        let gradientLen = (gradient.x.powi(2) + gradient.y.powi(2)).sqrt();
-        gradient.x /= gradientLen;
-        gradient.y /= gradientLen;
+        let gradient_len = (gradient.x.powi(2) + gradient.y.powi(2)).sqrt();
+        gradient.x /= gradient_len;
+        gradient.y /= gradient_len;
 
 
         table[index] = gradient;
@@ -143,16 +143,16 @@ fn shuffle(arr: &mut [u32]) {
 
     let mut rng = rand::thread_rng();
     let mut i = arr.len() / 2 - 1;
-    let mut randIndex;
+    let mut rand_index;
 
     while i > 0 {
-        randIndex = rng.gen_range(0..i+1);
+        rand_index = rng.gen_range(0..i+1);
 
         //println!("before swap for i={}:", i);
         //println!("arr[i]={}, arr[randIndex]={}", arr[i], arr[randIndex]);
 
         // swap arr[i] with arr[randIndex]
-        arr.swap(i, randIndex);
+        arr.swap(i, rand_index);
 
         //println!("after swap:");
         //println!("arr[i]={}, arr[randIndex]={}", arr[i], arr[randIndex]);
@@ -177,25 +177,25 @@ fn lerp(weight: f32, dot1: f32, dot2: f32) -> f32 {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let permTable = createPermutationTable();
-    let gradTable = createGradientTable();
+    let perm_table = create_permutation_table();
+    let grad_table = create_gradient_table();
 
     // need to generate perm and grid table here to keep pseudorandomness
     //println!("\nnoise val of (0.0, 0.0): {}", noise2D(0.0, 0.0, permTable, gradTable));
     //println!("noise val of (0.1, 0.1): {}", noise2D(0.1, 0.1, permTable, gradTable));
 
-    let mut noiseMap = [[0.0; MAPSIZE]; MAPSIZE];
-    let mut heightMap = [['$'; MAPSIZE]; MAPSIZE];
+    let mut noise_map = [[0.0; MAPSIZE]; MAPSIZE];
+    let mut height_map = [['$'; MAPSIZE]; MAPSIZE];
 
     // generate 2D noisemap of SIZExSIZE dimensions
     for y in 0..MAPSIZE {
         for x in 0..MAPSIZE {
-            let mut noise = noise2D(x as f32 * 0.01, y as f32 * 0.01, permTable, gradTable);
+            let mut noise = noise_2d(x as f32 * 0.01, y as f32 * 0.01, perm_table, grad_table);
 
             noise += 1.0;
             noise /= 2.0;
 
-            noiseMap[y][x] = noise;
+            noise_map[y][x] = noise;
         }
         
     }
@@ -209,52 +209,52 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
 
     // convert each noise float value to a text tile for viewability
-    let mut deepWaterCount = 0;
-    let mut waterCount = 0;
-    let mut landCount = 0;
-    let mut hillCount = 0;
-    let mut mountainCount = 0;
+    let mut deep_water_count = 0;
+    let mut water_count = 0;
+    let mut land_count = 0;
+    let mut hill_count = 0;
+    let mut mountain_count = 0;
 
     for y in 0..MAPSIZE {
         for x in 0..MAPSIZE {
             let tile;
             let color;  // color for generating a very basic terrain map
             let color2;         // more accurate color from each float val
-            let noise = noiseMap[y][x];
+            let noise = noise_map[y][x];
             if noise < 0.2 {
                 //tile = '*';
                 tile = 'W'; // deep water
                 color = Rgb([16, 41, 115]); // dark blue
 
-                deepWaterCount += 1;
+                deep_water_count += 1;
             } else if noise >= 0.2 && noise < 0.4 {
                 //tile = '+';
                 tile = 'w'; // water
                 color = Rgb([45, 83, 196]); // blue
-                waterCount += 1;
+                water_count += 1;
             } else if noise >= 0.4 && noise < 0.6 {
                 //tile = '+';
                 tile = 'L'; // land
                 color = Rgb([18, 135, 31]); // green
-                landCount += 1;
+                land_count += 1;
             } else if noise >= 0.6 && noise < 0.8 {
                 //tile = 'O';
                 tile = 'H'; // hill
                 color = Rgb([84, 46, 13]); // brown
-                hillCount += 1;
+                hill_count += 1;
             } else {
                 //tile = '^';
                 tile = 'M'; // mountain
                 color = Rgb([65, 65, 65]); // gray
-                mountainCount += 1;
+                mountain_count += 1;
             }
             
-            let noiseRGB = (noise * 255.0).round() as u8;
-            color2 = Rgb([noiseRGB, noiseRGB, noiseRGB]);
+            let noise_rgb = (noise * 255.0).round() as u8;
+            color2 = Rgb([noise_rgb, noise_rgb, noise_rgb]);
 
             img.put_pixel(x.try_into().unwrap(), y.try_into().unwrap(), color);
             img2.put_pixel(x.try_into().unwrap(), y.try_into().unwrap(), color2);
-            heightMap[y][x] = tile;
+            height_map[y][x] = tile;
         }
     }
 
@@ -262,9 +262,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut file = LineWriter::new(file);
     
 
-    for row in heightMap {
-        let tileRow = String::from_iter(row);
-        file.write_all(tileRow.as_bytes())?;
+    for row in height_map {
+        let tile_row = String::from_iter(row);
+        file.write_all(tile_row.as_bytes())?;
         file.write_all(b"\n")?;
     }
 
@@ -283,11 +283,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     
 
-    println!("Count of deep water tiles: {}", deepWaterCount);
-    println!("Count of water tiles: {}", waterCount);
-    println!("Count of land tiles: {}", landCount);
-    println!("Count of hill tiles: {}", hillCount);
-    println!("Count of mountain tiles: {}", mountainCount);
+    println!("Count of deep water tiles: {}", deep_water_count);
+    println!("Count of water tiles: {}", water_count);
+    println!("Count of land tiles: {}", land_count);
+    println!("Count of hill tiles: {}", hill_count);
+    println!("Count of mountain tiles: {}", mountain_count);
 
     Ok(())
 
